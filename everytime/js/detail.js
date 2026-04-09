a2e2a2621bc3acd3789fbc2eb0e3c55001d49fe1
unknown <jooeeeh17@duksung.ac.kr>
2026-04-09 14:43:00 +0900

    중간 저장

--- everytime/js/detail.js
+++ everytime/js/detail.js
@@ -1,32 +1,9 @@
 const likeBtn = document.getElementById("likeBtn");
 const scrapBtn = document.getElementById("scrapBtn");
 
-const likecount = document.getElementById("likecount");
-const scrapcount = document.getElementById("scrapcount");
+let count = 0;
 
-let liked = false;
-let scrapped = false;
-
-likeBtn.addEventListener("click", function () {
-  if (!liked) {
-    let currentLike = Number(likecount.textContent);
-    likecount.textContent = currentLike + 1;
-    liked = true;
-  }
-});
-
-scrapBtn.addEventListener("click", function () {
-  if (!scrapped) {
-    let currentScrap = Number(scrapcount.textContent);
-    scrapcount.textContent = currentScrap + 1;
-    scrapBtn.innerHTML =
-      '<img src="../img/container.articles.buttons.scrap.png" alt="스크랩이미지"> 스크랩 취소';
-    scrapped = true;
-  } else {
-    let currentScrap = Number(scrapcount.textContent);
-    scrapcount.textContent = currentScrap - 1;
-    scrapBtn.innerHTML =
-      '<img src="../img/container.articles.buttons.scrap.png" alt="스크랩이미지"> 스크랩';
-    scrapped = false;
-  }
+button.addEventListener("click", function () {
+  count++;
+  likeBtn.textContent = count;
 });
--- everytime/js/main.js
+++ everytime/js/main.js
@@ -1,5 +1,13 @@
-const btn = document.getElementById("writebtn");
+window.onload = function() {
+    const writeBtn = document.getElementById('writebtn'); // 연필 버튼
+    const postSection = document.getElementById('post-section'); // 숨겨둔 post 영역
+    const originalBox = document.querySelector('.secret-box3'); // 원래 있던 연필 박스
 
-btn.addEventListener("click", function () {
-  window.location.href = "post.html";
-});
+    // 버튼이 정상적으로 존재하는지 확인 후 이벤트 등록
+    if (writeBtn) {
+        writeBtn.addEventListener('click', function() {
+            originalBox.style.display = 'none';    
+            postSection.style.display = 'block';   
+        });
+    }
+};
